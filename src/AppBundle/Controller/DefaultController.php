cc31f64db417da90d00da03ae8ea0f926261203a
PLUS100\j.didi <jawad@plus100.eu>
2018-07-06 15:07:10 +0200

    Revert "test"
    This reverts commit 46b4a1fe9871b882030ce923b610e932e06b3c77.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,4 +19,28 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route ("/nlbe", name="nlbe")
+     */
+    public function nlbe(Request $request){
+
+
+        //Verwijzing naar formulier
+
+        return new Response($this->renderView('nlbe/index.html.twig'));
+
+    }
+
+    /**
+     * @Route ("/duitsland", name="duitsland")
+     */
+    public function duitsland(Request $request){
+
+
+        //Verwijzing naar formulier
+
+        return new Response($this->renderView('duitsland/index.html.twig'));
+
+    }
 }
